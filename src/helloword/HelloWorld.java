1d82cfebeeb9707c01bd44599abcf427c889ab5c
14667 <14667@LAPTOP-23BVO7I6>
2019-12-18 09:45:06 +0800

    test

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,7 +3,7 @@ package helloword;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println("Hello World修改一下");
 	}
 
 }
